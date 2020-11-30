28fbb06b41b2519d23e71a9d2607a780c066735e
林俊安 <linjunan@linjunande-MacBook-Air.local>
2020-11-30 19:23:43 +0900

    new SC

--- contract/DIDsRegistry_20201120.sol
+++ contract/DIDsRegistry_20201120.sol
@@ -3,12 +3,9 @@ pragma solidity ^0.7.0;
 contract DIDRegistry {
     
     mapping(address => address) public owners;  //identity => ownerAddress
-    //mapping(address => mapping(bytes32 => mapping(address => uint))) public delegates;  //identity => delegateUrl => delegetAddress
     mapping(address => bytes32) public DIDsDocumentUrls; //identity => DIDsDocumentsUrlHash
     mapping(address => mapping(bytes32 => bytes32)) public DIDsDocumentInfo; //mapping identity => urlHash => docInfoHash
-    //mapping(address => uint) public changed;
-    //mapping(address => uint) public nonce;
-    
+    mapping(address => mapping(bytes32 => uint)) public DIDsUrlExpiration; //mapping identity => urlHash => docInfoHash
     
     modifier onlyOwner(address identity, address actor) {
         require (actor == owners[identity]);
@@ -24,12 +21,11 @@ contract DIDRegistry {
         uint timestamp
     );
     
-    event DocumentChanged(
+    event DocumentUrlChanged(
         address indexed identity,
         bytes32 UrlHash,
         uint timestamp
     );
-    
     
     event DIDOwnerChanged(
         address indexed identity,
@@ -50,10 +46,11 @@ contract DIDRegistry {
     }
     
     //map the identity to owner address;map the identity to DIDUrl
-    function registerIdentity(address _identity, bytes32 _url_hash) public returns(bool) {
+    function registerIdentity(address _identity, string memory _url) public returns(bool) {
         owners[_identity] = msg.sender;
-        DIDsDocumentUrls[_identity] = _url_hash;
-        emit Registered(_identity, msg.sender, _url_hash, block.timestamp);
+        //(keccak256(abi.encodePacked(_url, msg.sender, random_number))) ;
+        DIDsDocumentUrls[_identity] = (keccak256(abi.encodePacked(_url)));
+        emit Registered(_identity, msg.sender, DIDsDocumentUrls[_identity], block.timestamp);
         return true;
     }
     
@@ -69,20 +66,27 @@ contract DIDRegistry {
         emit DIDOwnerChanged(_identity, _newOwner, block.timestamp);
     }
     
-    function changeDIDDocument(address _identity, bytes32 _previous_url_hash, bytes32 _new_hash) public onlyOwner(_identity, msg.sender) {
-        require(DIDsDocumentUrls[_identity] == _previous_url_hash);
-        DIDsDocumentUrls[_identity] == _new_hash;
-        emit DocumentChanged(_identity, _new_hash, block.timestamp);
+    function changeDIDDocumentUrl(address _identity, string memory _previous_url, string memory _new_url) public onlyOwner(_identity, msg.sender) {
+        require(DIDsDocumentUrls[_identity] == (keccak256(abi.encodePacked(_previous_url))));
+        DIDsDocumentUrls[_identity] == (keccak256(abi.encodePacked(_new_url)));
+        emit DocumentUrlChanged(_identity, DIDsDocumentUrls[_identity], block.timestamp);
     }
     
     //map the identity to the document information hash
-    function setDocumentInfo(address _identity, address _actor, bytes32 _url_hash, bytes32 _docHash, uint _validity) internal onlyOwner(_identity, _actor) {
-        DIDsDocumentInfo[_identity][_url_hash] = _docHash;
-        emit DocumentInfoChanged(_identity, _url_hash, _docHash, block.timestamp + _validity, block.timestamp);
+    function setDocumentInfo(address _identity, string memory _url, bytes32 _docHash, uint _validity) public onlyOwner(_identity, msg.sender) {
+        bytes32 url = (keccak256(abi.encodePacked(_url)));
+        DIDsDocumentInfo[_identity][url] = _docHash;
+        DIDsUrlExpiration[_identity][url]= block.timestamp + _validity;
+        emit DocumentInfoChanged(_identity, url, _docHash, block.timestamp + _validity, block.timestamp);
       }
+      
+   function validateUrl(address _identity,string memory _url) public view returns(bool){
+       bytes32 url = (keccak256(abi.encodePacked(_url)));
+       if(DIDsDocumentUrls[_identity] == url){
+           return true;
+       } else {
+           return false;
+       }
+   }
     
-    //
-    function setDocumentInfo(address _identity, bytes32 _url_hash, bytes32 _docHash, uint _validity) public {
-        setDocumentInfo(_identity, msg.sender, _url_hash, _docHash, _validity);
-    }
 }
